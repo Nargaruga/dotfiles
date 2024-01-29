14ac6ae3e9ce2d27e1616ec1f2bcb473b02db983
Leonardo Da Pozzo <l.dapozzo398@gmail.com>
2024-01-29 12:45:41 +0100

    added aerial.nvim, removed nvim-remote-containers

--- nvim/lua/leo/plugins.lua
+++ nvim/lua/leo/plugins.lua
@@ -52,16 +52,26 @@ local plugins = {
     },
 
     -- Diagnostics
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
     },
 
-    -- Container developement
-    'jamestthompson3/nvim-remote-containers',
+    -- Code navigation
+    {
+        'stevearc/aerial.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
 
     -- Better undo
     'mbbill/undotree',
--- nvim/lua/leo/plugins_config.lua
+++ nvim/lua/leo/plugins_config.lua
@@ -49,7 +49,7 @@ cmp.setup({
     }
 })
 
-require("lspconfig").lua_ls.setup {
+local lspconfig = require("lspconfig").lua_ls.setup {
     settings = {
         Lua = {
             diagnostics = {
@@ -59,6 +59,17 @@ require("lspconfig").lua_ls.setup {
         },
     },
 }
+
+require("aerial").setup({
+    -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+    on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+    end,
+})
+-- You probably also want to set a keymap to toggle aerial
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 
 -- Autoclose
 require("autoclose").setup()
